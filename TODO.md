19d30ffe785230f3782039665cc97c68e1203858
alexace21 <alex.ace7ov@gmail.com>
2022-06-19 20:50:08 +0300

    Initialize project
    1. Initialize project.
        * npm init --yes
        * install dependencies / nodemon, express, express-handlebars /
        * Modify package.json
    2. Add resources. - html, css, views, images
    
    3. Express config.
        * body parser.
        * static path
    4. Configure express-handlebars.
    5. Add router.
    6. Add home controller.
    7. Setup layouts.
    8. Add home view/template.
    9. Fix static/public assets and paths.

---
+++ TODO.md
@@ -0,0 +1,17 @@
+# Tasks
+
+1. Initialize project.
+    * npm init --yes
+    * install dependencies / nodemon, express, express-handlebars /
+    * Modify package.json
+2. Add resources. - html, css, views, images
+
+3. Express config.
+    * body parser.
+    * static path
+4. Configure express-handlebars.
+5. Add router.
+6. Add home controller.
+7. Setup layouts.
+8. Add home view/template.
+9. Fix static/public assets and paths.
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+
+const { PORT } = require('./config/environment');
+const routes = require('./routes');
+
+const app = express();
+
+app.engine('hbs', hbs.engine({
+    extname: 'hbs'
+}));
+
+app.set('view engine', 'hbs');
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.static('public'));
+app.use(routes);
+
+app.listen(PORT, () => console.log(`Application is running on port ${PORT}...`));
